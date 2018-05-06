fe24cfc02cb01730ca10153f54ea1d72c86c97f5
Jingya Wu <jw057@bucknell.edu>
2018-05-06 15:49:28 -0400

    Add hello world

---
+++ project/report/code_samples/hello.go
@@ -0,0 +1,17 @@
+/*
+Language: Go
+
+Instructions:
+Run the command 'go run hello.go'.
+Should see output 'hello, world'.
+
+I already know how to write this.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("hello, world\n")
+}
